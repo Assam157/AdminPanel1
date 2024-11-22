de9aa2067fa019caa5283dc3a882e0a1a43c8721
Assam157 <asasm8106@gmail.com>
2024-11-22 16:39:35 +0530

    Update LogInMayukh.jsx

--- src/components/LogInMayukh.jsx
+++ src/components/LogInMayukh.jsx
@@ -26,7 +26,7 @@ const LogInMayukh = () => {
     setSucces(false);
 
     try{
-      const response=await fetch("http://localhost:3001/LogInMayukh",{
+      const response=await fetch("https://enigmatic-shelf-01881-15c9cb2f80b1.herokuapp.com/LogInMayukh",{
         method:'POST',
         headers:{
           'Content-Type':'application/json',
