d145450fc8852dce4ce94312cdeabc55d73f1344
Assam157 <asasm8106@gmail.com>
2025-03-13 15:51:04 +0530

    Update LogInMayukh.jsx

--- src/components/LogInMayukh.jsx
+++ src/components/LogInMayukh.jsx
@@ -26,7 +26,7 @@ const LogInMayukh = () => {
     setSucces(false);
 
     try{
-      const response=await fetch("https://shoppingpage-in.com/LogInMayukh",{
+      const response=await fetch("https://shoppingpage.in/LogInMayukh",{
         method:'POST',
         headers:{
           'Content-Type':'application/json',
