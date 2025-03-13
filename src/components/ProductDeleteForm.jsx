8ce4a171f58635a25765b91cc7f50c3196a09e3d
Assam157 <asasm8106@gmail.com>
2025-03-13 15:45:45 +0530

    Update ProductDeleteForm.jsx

--- src/components/ProductDeleteForm.jsx
+++ src/components/ProductDeleteForm.jsx
@@ -27,7 +27,7 @@ const ProductDelete = () => {
 
         try {
             // Make sure to use DELETE method here
-            const response = await fetch("https://shoppingpage-in.preview-domain.com/delete", {
+            const response = await fetch("https://shoppingpage-in.com/delete", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
